984175df4cb07d58ef0b76c2474558d5860a8761
Adam Harris <adamgrantharris14@gmail.com>
2018-03-08 10:38:50 -0600

    Added page changing functionality

---
+++ assets/javascript/script.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+
+    const navTabs = document.getElementsByClassName('nav-item');
+
+    const pages = document.getElementsByClassName('page');
+
+    for (let i = 0; i < navTabs.length; i++) {
+
+        navTabs[i].addEventListener('click', changePage);
+    }
+
+    function changePage(e) {
+
+        let text = e.target.textContent;
+
+        for (let i = 0; i < pages.length; i++) {
+
+            text.toLowerCase() === pages[i].attributes[2].nodeValue ?
+                pages[i].style.display = 'initial' :
+                pages[i].style.display = 'none';
+        }
+    }
+
+    function pageLoadSet() {
+
+        for (let i = 0; i < pages.length; i++) {
+
+            pages[i].attributes[2].nodeValue === 'home' ?
+                pages[i].style.display = 'initial' :
+                pages[i].style.display = 'none';
+        }
+    }
+
+    function init() {
+
+        pageLoadSet();
+    }
+
+    init();
+};
