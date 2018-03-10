e0759461df5f4c06fdccc3c7c0fd665402571d4d
Gregory Leeker <gregoryleeker@Gregorys-MacBook-Air.local>
2018-03-10 13:54:43 -0600

    css and bootstrap added to profile and shows pages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -45,3 +45,12 @@ var nodeList = anime({
   targets: els,
   translateX: 250
 });
+var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
